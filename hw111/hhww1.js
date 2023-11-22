5f787458e2bbbef5ceb419c8ee8b229c94bd6d54
Yarkndr <kondratenko201723@gamil.com>
2023-11-22 10:03:33 +0200

    Create NewJS fileHW1

---
+++ hw111/hhww1.js
@@ -0,0 +1,60 @@
+
+let hello="Hello"
+console.log(hello);
+let owu = "owu"
+console.log(owu);
+let com="com"
+console.log(com);
+let ua="ua";
+console.log(ua);
+
+let n1=1;
+console.log(n1);
+let n2=10;
+console.log(n2);
+let n3=-999;
+console.log(n3);
+let n4=123;
+console.log(n4);
+let n5=3.14;
+console.log(n5);
+let n6=2.7;
+console.log(n6);
+let n7=16;
+console.log(n7);
+
+let atrue=88>87;
+console.log(atrue);
+let afalse=100<99;
+console.log(afalse);
+
+//------------
+
+let firstName="Yaroslav";
+let middleName="Kondratenko";
+let lastName="Oleksandrovich";
+let space=" "
+
+let person='Yaroslav'+space+'Kondratenko'+space+'Oleksandrovich';
+console.log(person);
+
+//---------------
+let a=100;
+console.log(typeof a);
+
+let b="100";
+console.log(typeof b);
+
+let c=true;
+console.log(typeof c);
+
+//----------------
+
+let name=prompt('Name', 'Yaroslav');
+console.log(name);
+
+let lastname=prompt('Lastname','Aleksandrovich');
+console.log(lastname);
+
+let age=prompt('Age','28');
+console.log(age);
